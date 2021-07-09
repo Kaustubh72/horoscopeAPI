b8d279cd7b7edee8ab70eb943cfbb74155b84c85
Kaustubh72 <ks04071999@gmail.com>
2021-07-09 16:40:48 +0530

    added date-wise range

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(function (req, res, next) {
 app.get('/', async function(req, res) 
 {
     var prediction="";
+    
     var horoscope = ["none","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn","Aquarius","Pisces"];
+    let arr = ["null","Mar 21 – Apr 20","Apr 21 – May 21","May 22 – Jun 21","Jun 22 – Jul 23","Jul 24 – Aug 23","Aug 24 – Sep 23","Sep 24 – Oct 23","Oct 24 – Nov 22","Nov 23 – Dec 21","Dec 22 – Jan 20","Jan 21 – Feb 19","Feb 20 – Mar 20"];
     var json =[];
     for(id=1;id<13;id++){
       url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign='+id;
@@ -21,7 +23,7 @@ app.get('/', async function(req, res)
             $ = cheerio.load(html);
             prediction = $('div.main-horoscope > p').text();
             resolve({
-                id: id,
+                id: arr[id],
                 horoscope: horoscope[id],
                 prediction: prediction,
             });
